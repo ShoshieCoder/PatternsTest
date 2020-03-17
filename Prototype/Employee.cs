e1a0f98e881b0b1f57026d761714050873804b62
ShoshieCoder <46136018+ShoshieCoder@users.noreply.github.com>
2020-03-17 16:14:52 +0100

    Add files via upload

---
+++ Prototype/Employee.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProtoType
+{
+    public class Employee : IProto<Employee>
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int Salary { get; set; }
+
+        public Employee(int id, string name, int age, int salary)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Age = age;
+            this.Salary = salary;
+        }
+        public Employee Clone()
+        {
+            Employee result = new Employee(
+                this.Id,
+                this.Name,
+                this.Age,
+                this.Salary
+           );
+            return result;
+        }
+        public void Print()
+        {
+            Console.WriteLine($"id={Id},Name={Name},Age={Age},Salary={Salary}");
+        }
+        public override string ToString()
+        {
+            return $"{Id} {Name} {Age} {Salary}";
+        }
+    }
+}
---
+++ Prototype/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProtoType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> empList = new List<Employee>{
+                new Employee (1,"Zrobavel", 55, 3000 ),
+                new Employee (2, "Zrobavela", 25, 5000),
+                new Employee (3, "Sahar", 28, 8000),
+                new Employee (4, "Turgeman", 28, 8000)
+            };
+
+            List<Employee> cloneList = CloneList(empList);
+            PrintList(cloneList);
+            List<Employee> sortList = SortList(cloneList);
+            PrintList(sortList);
+        }
+
+        static List<Employee> CloneList(List<Employee> empList)
+        {
+            List<Employee> empCloneList = new List<Employee>();
+            foreach (Employee em in empList)
+            {
+                empCloneList.Add(em.Clone());
+            }
+            return empCloneList;
+        }
+
+        static List<Employee> SortList(List<Employee> empList)
+        {
+            int Count = 200; // empList.Count when not dubugging
+            if (Count < 100)
+                return empList.OrderBy(e => e.Id).ToList<Employee>();
+            else if (Count <= 200)
+                return empList.OrderBy(e => e.Name).ToList<Employee>();
+            else if (Count >= 300)
+                return empList.OrderBy(e => e.Salary).ToList<Employee>();
+            else
+            {
+                Console.WriteLine("Couldn't sort");
+                return null;
+            }
+                
+        }
+
+        static void PrintList(List<Employee> empList)
+        {
+            foreach (Employee e in empList)
+            {
+                e.Print();
+            }
+        }
+    }
+}
